673ad70575f618c8cf899d0ff62a135020c6d393
Michele Kempinsky <mkempinsky@icloud.com>
2018-12-31 21:45:52 -0800

    first commit

---
+++ pages/home.js
@@ -0,0 +1,8 @@
+const Home = props => {
+    return (
+        <div>
+            <h3>hello from home page</h3>
+        </div>
+    )
+}
+export default Home;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+
+
+const Index = props => {
+    return (
+        <div>index
+        
+        <Link href="home">
+            <a>Home Page</a>
+        </Link>
+        </div>
+    )
+}
+export default Index;
