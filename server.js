dde90921e9e5749b823278171b5797ab79f60036
Michele Kempinsky <mkempinsky@icloud.com>
2019-12-06 15:13:42 -0800

    add cors premission

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ app.prepare()
         server.use(compression());
 
         /**
+         * CORS
+         */
+        server.use(function(req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
+            next();
+        });
+
+        /**
          * Remove trailing slash from URLs
          */
         server.use((req, res, next) => {
